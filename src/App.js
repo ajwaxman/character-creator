17d4f5556cf69084217cd01299fe24132d473ad0
Adam Waxman <awaxman11@gmail.com>
2022-11-10 09:13:32 -0500

    Character project exercises 1-4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+const perspective = {
+  position: 'fixed',
+  background: 'hsl(195deg, 20%, 86%)',
+  height: '40%',
+  width: '100%',
+  bottom: 0,
+  'z-index': '-1', 
+}
+
 function App() {
   return (
     <>
       <CharacterEditor />
+      <div style={perspective}></div>
       <Footer />
     </>
   );
